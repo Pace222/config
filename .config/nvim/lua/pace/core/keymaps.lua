9ec8309cd93678da0b805c4123a8e48d6e1c8de4
Pierugo Pace <pierugo.pace@switch.ch>
2025-01-17 23:48:29 +0100

    Edited keymaps

--- .config/nvim/lua/pace/core/keymaps.lua
+++ .config/nvim/lua/pace/core/keymaps.lua
@@ -6,7 +6,8 @@ local keymap = vim.keymap -- for conciseness
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
+
+keymap.set("n", "U", "<C-r>", { desc = "Redo" })
 
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
@@ -17,7 +18,7 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
+keymap.set("n", "<leader>sz", "<cmd>tab split<CR>", { desc = "Zoom on the current split" }) -- zoom on the current split
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
