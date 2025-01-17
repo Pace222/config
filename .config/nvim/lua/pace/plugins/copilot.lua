90ce206c3ae31e7d66f1da73da9db7449bdf9884
Pierugo Pace <pierugo.pace@switch.ch>
2025-01-17 23:48:50 +0100

    Changed copilot keymaps

--- .config/nvim/lua/pace/plugins/copilot.lua
+++ .config/nvim/lua/pace/plugins/copilot.lua
@@ -1,4 +1,13 @@
 return {
   "github/copilot.vim",
   event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local keymap = vim.keymap -- for conciseness
+    keymap.set("i", "$", 'copilot#Accept("$")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
+    keymap.set("i", "<C-e>", "<Plug>(copilot-dismiss)")
+  end,
 }
